6fd7e925a2c3fd387302a77092e341ba929ff7fb
Steve Göring <steve.goering@tu-ilmenau.de>
2023-04-27 14:26:59 +0200

    changes

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p static/images
 cd static/images
-wget "https://ftp.tu-ilmenau.de/hpc-private/ei/avt/sophoappeal/images_data.zip"
+wget "https://resdata.tu-ilmenau.de/public/ei/avt/sophoappeal/images_data.zip"
 # extract
 unzip images_data.zip
 wget "https://raw.githubusercontent.com/Telecommunication-Telemedia-Assessment/sophoappeal_images/main/likes_views.json"
